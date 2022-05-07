5d9c6f1ce9f4f87d4073532850ce818dbbced0db
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-05-07 09:22:25 +0900

    add: matrixt add

---
+++ homework/matrix/src/MatAddStudent20191765.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class MatAddStudent20191765 {
+
+	public static class MatAddMapper extends Mapper<Object, Text, Text, IntWritable>
+	{
+		private IntWritable i_value = new IntWritable();
+		private Text word = new Text();
+		
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			String row_id = itr.nextToken().trim();
+			String col_id = itr.nextToken().trim();
+			i_value.set(Integer.parseInt(itr.nextToken().trim()));
+
+			word.set(row_id + "," + col_id);
+			context.write(word, i_value);
+		}
+	}
+
+	public static class MatrixAddReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private InWritable result = new IntWritable;
+		
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int sum = 0;
+
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2)
+		{
+			System.exit(2);
+		}
+
+		Job job = new Job(conf, "matrixtadd student20191765");
+		job.setJarByClass(MatAddStudent20191765.class);
+		job.setMapperClass(MatAddMapper.class);
+		job.setCombinerClass(MatAddReducer.class);
+		job.setReducerClass(MatAddReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
