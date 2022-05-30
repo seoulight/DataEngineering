1edc8cc151e57f13be27d0a3fbb0b5e34cf76fba
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-05-30 12:37:48 +0900

    update: output value

--- top-k/src/IMDBStudent20191765.java
+++ top-k/src/IMDBStudent20191765.java
@@ -82,7 +82,7 @@ public class IMDBStudent20191765 {
 		job.setMapperClass(IMDBMapper.class);
 		job.setReducerClass(IMDBReducer.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(DoubleWritable.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 
